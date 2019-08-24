db7b6708c17cc3a4d9c54d09ecac9fede1bae1fb
Davidood <dwhywood@gmail.com>
2019-08-24 11:03:11 -0500

    Move drag handlers to its own method

--- src/ToDoApp.js
+++ src/ToDoApp.js
@@ -9,37 +9,45 @@ export default class ToDoApp {
     this.container = createElement('div', { class: 'todo', id: 'todo' });
 
     this.container.addEventListener('dragover', (e) => {
-      e.preventDefault();
+      this.handleDragOver(e);
     });
 
     this.container.addEventListener('drop', (e) => {
-      if (!e.target.classList.contains('dropzone')) {
-        return;
-      }
-
-      let { target } = e; 
-      let { buckets } = this; 
-      let data = JSON.parse(e.dataTransfer.getData("application/data"));
-      
-      let draggedElement = document.getElementById(`task-${data.id}`);
-      let oldBucketId = data.dataFor;
-      let newBucketId = target.dataset.for;
-
-      let oldBucket = buckets.find((bucket) => oldBucketId === bucket.id);
-
-      let task = oldBucket.getTask(data.id);
-      oldBucket.removeTask(data.id);
-
-      let newBucket = buckets.find((bucket) => newBucketId === bucket.id);
-
-      newBucket.addTask(Object.assign(data, { dataFor: newBucketId}));
-      draggedElement.dataset.for = newBucketId;
-
-      target.appendChild(draggedElement);
+      this.handleDrop(e);
     });
 
   }
 
+  handleDragOver(e) {
+    e.preventDefault(); 
+  }
+  
+  handleDrop(e) {
+    if (!e.target.classList.contains('dropzone')) {
+      return;
+    }
+
+    let { target } = e; 
+    let { buckets } = this; 
+    let data = JSON.parse(e.dataTransfer.getData("application/data"));
+    
+    let draggedElement = document.getElementById(`task-${data.id}`);
+    let oldBucketId = data.dataFor;
+    let newBucketId = target.dataset.for;
+
+    let oldBucket = buckets.find((bucket) => oldBucketId === bucket.id);
+
+    let task = oldBucket.getTask(data.id);
+    oldBucket.removeTask(data.id);
+
+    let newBucket = buckets.find((bucket) => newBucketId === bucket.id);
+
+    newBucket.addTask(Object.assign(data, { dataFor: newBucketId}));
+    draggedElement.dataset.for = newBucketId;
+
+    target.appendChild(draggedElement);
+  }
+  
   createBucket() {
     let { buckets } = this;
     let bucket = new Bucket({ title: ''});
