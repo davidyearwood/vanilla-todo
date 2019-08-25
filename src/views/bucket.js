b9e27b1db100b904934607d98308c36f5aed9456
Davidood <dwhywood@gmail.com>
2019-08-25 12:07:54 -0500

    Create bucket form component

--- src/views/bucket.js
+++ src/views/bucket.js
@@ -1,16 +1,56 @@
 import createElement from '../utils/createElement.js';
 
 export function BucketComponent(props) {
-  const { id, title } = props;
+  const {
+    id,
+    title
+  } = props;
 
   return createElement(
-    'section',
-    {
+    'section', {
       class: 'bucket',
       id: `bucket-${id}`,
       'data-id': id,
       'data-type': 'bucket'
-    },
-    [createElement('h1', { class: 'bucket__title' }, title)]
+    }, [createElement('h1', {
+      class: 'bucket__title'
+    }, title)]
+  );
+}
+
+export function BucketFormComponent(props) {
+  let {
+    id,
+  } = props;
+
+  return createElement(
+    'form', {
+      class: 'bucket-form',
+      'data-id': id,
+      'data-type': 'form',
+      id: `bucket-form-${id}`,
+    }, [
+      createElement('label', {
+        for: `bucket-form-title-${id}`
+      }, 'Title'),
+      createElement('input', {
+        type: 'text',
+        class: 'bucket-form__input',
+        id: `bucket-form-title-${id}`,
+        value: '',
+        name: 'title'
+      }),
+
+      createElement(
+        'button', {
+          class: 'task-form__button',
+          'data-action': 'create-bucket',
+          'data-for': props.id,
+          'data-type': 'form'
+        },
+        'Create Bucket'
+      )
+
+    ]
   );
 }
